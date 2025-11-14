08782c42796e507acbf43e559c162ef3ef805111
ivanchor <ichor85@gmail.com>
2025-11-13 20:26:41 -0800

    Added functionality for calculator

--- script.js
+++ script.js
@@ -16,44 +16,77 @@ const multiply = (num1, num2) => {
 
 const operate = (num1, num2, operator) => {
     switch (operator){
-        case operator == "+":
-            add(num1, num2);
+        case "+":
+            return add(num1, num2);
             break;
-        case operator == "-":
-            subtract(num1, num2);
+        case "-":
+            return subtract(num1, num2);
             break;
-        case operator == "/":
-            divide(num1, num2);
+        case "/":
+            return divide(num1, num2);
             break;
-        case operator == "*":
-            multiply(num1, num2);
+        case "*":
+            return multiply(num1, num2);
             break;    
     }   
 }
 
 // Calculator Functionality
 
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display")
 const numbers = document.querySelectorAll(".number")
+
+function checkDisplay(){
+    return isNaN(display.textContent)
+}
+
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if(!(number1 == null)){
+            display.textContent = operator.textContent;
+            op = operator.textContent;
+            console.log("Operator: " + op)
+        }
+    })
+})
+
+
 
 let number1 = null;
 let number2 = null;
 let op = null;
 
+function displayNumber(number, digit){
+    if(number===null){
+        display.textContent = digit;
+    }
+    else{
+        if (display.textContent.length < 7) {
+            display.textContent += digit;
+        }
+    }
+    return display.textContent;
+}
+
 numbers.forEach(number => {
     number.addEventListener('click', ()=> {
-        if(number1===null){
-            display.textContent = number.textContent;
-            number1 = display.textContent;
+        if(checkDisplay()){
+            display.textContent = '';
+        }
+        
+        if(op == null){
+            number1 = displayNumber(number1, number.textContent)
         }
         else{
-            if (display.textContent.length < 7) {
-            display.textContent += number.textContent;
-            number1 = display.textContent;
-            }
+            number2 = displayNumber(number2, number.textContent)
         }
+        console.log("Number 1:" + number1)
+        console.log("Number 2:" + number2)
+        console.log(number.textContent) 
 
-        
+
         // console.log(number.textContent);
     })
 })
@@ -62,3 +95,16 @@ const clearButton = document.querySelector('.clear_button');
 clearButton.addEventListener('click', () => {
     display.textContent = ''; // clear the display
 });
+
+const enterButton = document.querySelector('.enter_button');
+enterButton.addEventListener('click', () => {
+    // console.log("Number 1:" + number1)
+    // console.log("Number 2:" + number2)
+    // console.log("Operator: " + op)
+    // console.log("Operation: " + operate(Number(number1), Number(number2), op))
+    number1 = operate(Number(number1), Number(number2), op);
+    number2 = null;
+    op = null;
+    display.textContent = number1.toString().slice(0, 7);;
+    
+})
